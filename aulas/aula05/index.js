ba7f5eaf3b5d94930d420950c56850a3fbbc5216
Davi Machado Atrock <daviatrock@gmail.com>
2024-03-12 22:44:52 -0300

    update testes aula05

--- aulas/aula05/index.js
+++ aulas/aula05/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const routerProdutos = require('./router');
+const routerProdutos = require("./router");
 const app = express();
 
 app.use(express.json());
+
 app.use(routerProdutos);
 
 app.listen(3000, function(){
-    console.log('API está ON!');
-});
+    console.log("API está ON!");
+})
 
 module.exports = app;
