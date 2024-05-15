d3fe5e3a830dd51e74178de68977b161fb7e484c
Davi Machado Atrock <daviatrock@gmail.com>
2024-05-14 21:41:08 -0300

    aula10

---
+++ aulas/aula10/tests/router_produtos.test.js
@@ -0,0 +1,43 @@
+const supertest = require("supertest");
+
+const app = require("../app");
+
+const request = supertest(app);
+
+let id = null;
+
+describe("API Loja Virtual", () => {
+  // test('Deve retornar 201 e um JSON no POST /produtos', async () => {
+  //   const response = await request.post('/produtos').send({nome: "banana", preco: 12.0});
+  //   expect(response.status).toBe(201);
+  //   expect(response.type).toBe('application/json');
+  //   console.log(id);
+  // });
+
+  test("Deve retornar 422 e um JSON no POST /produtos", async () => {
+    const response = await request.post("/produtos").send({});
+    expect(response.status).toBe(422);
+    expect(response.type).toBe("application/json");
+  });
+
+  test("Deve retornar 200 e um array no GET /produtos", async () => {
+    const response = await request.get("/produtos");
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+    if (response.body.length > 0) {
+      id = response.body[0]._id.toString();
+    }
+  });
+
+  test("Deve retornar 200 e um JSON no GET /produtos/id", async() => {
+    const response = await request.get(`/produtos/${id}`);
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  test("Deve retornar 404 e um JSON no GET /produto/id", async() => {
+    const response = await request.get("/produtos/id");
+    expect(response.status).toBe(404);
+    expect(response.type).toBe("application/json");
+  })
+});
